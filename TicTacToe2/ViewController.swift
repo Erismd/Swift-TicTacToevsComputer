3697b3fee5e5897c3bff2c651a853dead4933015
Eri Shimada <45121014+Erismd@users.noreply.github.com>
2020-12-24 08:55:26 +0900

    add button shadow

--- TicTacToe2/ViewController.swift
+++ TicTacToe2/ViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.swift
 //  TicTacToe2
 //
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowRadius = 15
+        button.layer.shadowOpacity = 1
+        button.layer.cornerRadius = 15
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
+
+extension UIColor {
+  convenience init(hex: String) {
+    var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    if cString.hasPrefix("#") {
+      cString.remove(at: cString.startIndex)
+    }
+    if (cString.count) != 6 {
+      self.init(red: CGFloat(255), green: CGFloat(255), blue: CGFloat(255), alpha: 1.0)
+    }
+    var rgbValue: UInt32 = 0
+    Scanner(string: cString).scanHexInt32(&rgbValue)
+    self.init(
+      red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+      alpha: CGFloat(1.0)
+    )
+  }
+}
